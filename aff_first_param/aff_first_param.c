aed72b36dda19a5e2ea0521d513b543998f6f9d7
ella578 <119518546+ella578@users.noreply.github.com>
2023-09-17 15:52:52 +0200

    Update aff_first_param.c

--- aff_first_param/aff_first_param.c
+++ aff_first_param/aff_first_param.c
@@ -16,10 +16,10 @@ int	main(int argc, char **argv)
 {
 	int	i;
 
-	i = -1;
+	i = 0;
 	if (argc >1)
-		while (argv[1][++i])
-			write(1, &argv[1][i], 1);
+		while (argv[1][i])
+			write(1, &argv[1][i++], 1);
 	write(1, "\n", 1);
 	return (0);
 }
