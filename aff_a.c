9969ade667acde28e227a3c52218c0b2714de3f0
ella578 <119518546+ella578@users.noreply.github.com>
2023-09-17 12:16:30 +0200

    Update aff_a.c

--- aff_a.c
+++ aff_a.c
@@ -13,22 +13,20 @@
 
 int	main(int	argc, char	**argv)
 {
-	int i = 0;
-	
-	if (argc == 2)
+	if (argc != 2)
+		write(1, "a", 1);
+	else
 	{
 		while (argv[1][i] != '\0')
 		{
-			if (argv[1][i] == 'a')
+			if (*argv[1])
 			{
-				write(1, "a\n", 2);
-				return(0);
+				write(1, "a\n", 1);
+				break;
 			}
-			i++;
+			argv[1]++;
 		}
-		write(1, "\n",1);
-		return (0);
 	}
-	write(1, "a\n",2);
+	write(1, "\n",1);
 	return (0);
 }
