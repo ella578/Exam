aece2a975f42b5020f89d6bb7966ea882d2bd171
ella578 <119518546+ella578@users.noreply.github.com>
2023-09-18 16:15:16 +0200

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -10,20 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, *c, 1);
-}
 void	ft_putstr(char *str)
 {
-	int i = 0;
+	int i;
+	
+	i = 0;
 
 	while (str[i])
 	{
-		ft_putchar(str[i]);
-		i++;
+		write (1, &str[i++], 1);
 	}
 }
