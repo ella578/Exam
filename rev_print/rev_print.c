ee3fea0276e0daabf386d7570a301c349d1e03ea
ella578 <119518546+ella578@users.noreply.github.com>
2023-09-20 14:43:23 +0200

    Update rev_print.c

--- rev_print/rev_print.c
+++ rev_print/rev_print.c
@@ -27,3 +27,34 @@ int	main(int argc, char *argv[])
 	write(1, "\n", 1);
 	return (0);
 }
+
+--------------------------------
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i] != '\0')
+		{
+			i++;
+		}
+		while (i >= 0)
+		{
+			ft_putchar(argv[1][i]);
+			i--;
+		}
+	}
+	ft_putchar('\n');
+	return (0);
+}
+
