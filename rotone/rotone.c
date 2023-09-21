e8a9dbf15b8fa670b3bbc49c3cd5aa00878566c5
ella578 <119518546+ella578@users.noreply.github.com>
2023-09-21 15:38:17 +0200

    Update rotone.c

--- rotone/rotone.c
+++ rotone/rotone.c
@@ -12,25 +12,33 @@
 
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int     main(int ac, char **av)
 {
-	int i;
+        int     i;
 
-	i = 0;
-	if (argc == 2)
-	{
-		while( argv[1][i])
-		{
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'Y')
-				argv[1][i] += 1;
-			if (argv[1][i] >= 'a' && argv[1][i] <= 'y')
-                                argv[1][i] += 1;
-			if (argv[1][i] == 'z' || argv[1][i] == 'Z')
-				argv[1][i] -= 25;
-			write (1, &argv[1][i], 1);
-			i += 1;		
-		}
-	}
-	write (1, "\n", 1);
-	return(0);
+        i = 0;
+        if (ac == 2)
+        {
+                while (av[1][i])
+                {
+                        if ((av[1][i] >= 'A' && av[1][i] <= 'Z') || (av[1][i] >= 'a' && av[1][i] <= 'z'))
+                        {
+                        if (av[1][i] == 'z' || av[1][i] == 'Z')
+                        {
+                        av[1][i] -= 25;
+                        write(1, &av[1][i], 1);
+                        }
+                        else
+                        {
+                        av[1][i] = av[1][i] + 1;
+                        write(1, &av[1][i], 1);
+                        }
+        }
+                        else
+                                write(1, &av[1][i], 1);
+        i++;
+        }
+        }
+        return(0);
 }
+
