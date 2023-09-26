79fae718f88344fdfd7a600d9dfdeb944fe1b6fb
ella578 <119518546+ella578@users.noreply.github.com>
2023-09-26 14:52:48 +0200

    Update union.c

--- union/union.c
+++ union/union.c
@@ -12,50 +12,34 @@
 
 #include <unistd.h>
 
-int check(char *str, char c, int n)
+int     check(char *s, int n, char c)
 {
-    int i = 0;
-    
-    while(i < n)
-    {
-        if(str[i] == c)
-            return(0);
-        i++;
-    }
-    return(1);
+        int i;
+
+        i = -1;
+        while (++i < n)
+                if (s[i] == c)
+                        return (0);
+        return (1);
 }
 
-void ft_strcat(char *s1, char *s2)
+void    ft_union(char *s1, char *s2)
 {
-    int i = 0;
-    int j = 0;
-    
-    while (s1[i] != '\0')
-        i++;
-    while (s2[j] != '\0')
-    {
-        s1[i] = s2[j];
-        i++;
-        j++;
-    }
-    s1[i] = '\0';
+        int     i= -1;
+        int     j= -1;
+
+        while (s1[++i])
+                if (check(s1, i, s1[i]))
+                        write(1, &s1[i], 1);
+        while (s2[++j])
+                if (check(s1, i, s2[j]) && check(s2, j, s2[j]))
+                        write(1, &s2[j], 1);
 }
 
-int main(int ac, char *av[])
+int             main(int ac, char **av)
 {
-    int i = 0;
-    
-    if (ac == 3)
-    {
-        ft_strcat(av[1], av[2]);
-        while (av[1][i] != '\0')
-        {
-            if(check(av[1], av[1][i], i))
-            {
-                write(1, &av[1][i], 1);
-            }
-            i++;
-        }
-    }
-    write(1, "\n", 1);
+        if (ac == 3)
+                ft_union(av[1], av[2]);
+        write(1, "\n", 1);
+        return (0);
 }
