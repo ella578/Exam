b4033efb6e3a8623abcc983bb835e6eeacef206b
ella578 <119518546+ella578@users.noreply.github.com>
2023-09-27 13:17:26 +0200

    Update ft_range.c

--- ft_range.c
+++ ft_range.c
@@ -14,24 +14,16 @@
 
 int *ft_range(int start, int end)
 {
-	int i = 0;
-	int len = abs((end - start)) + 1;
-	int *res = (int *)malloc(sizeof(int) * len);
-	
-	while (i < len)
-	{
-		if (start < end)
-		{
-			res[i] = start;
-			start++;
-			i++;
-		}
-		else
-		{
-			res[i] = start;
-			start--;
-			i++;
-		}
-	}
-        return (res);
+        int      len;
+        int      *range;
+
+        len = end >= start ? end - start : start - end;
+        if (!(range = (int *)malloc(sizeof(int) * len)))
+                return (NULL);
+
+        while (end != start)
+        *range++ = end > start ? start++ : start--;
+        *range = start;
+        return (range - len);
 }
+
