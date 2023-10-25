4e3d3c89e411d1d20aac5636024cef21a3cc8479
ella578 <119518546+ella578@users.noreply.github.com>
2023-10-25 13:50:35 +0200

    Update ft_range.c

--- ft_range.c
+++ ft_range.c
@@ -11,19 +11,50 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
-int *ft_range(int start, int end)
+int     *ft_range(int start, int end)
 {
-        int      len;
-        int      *range;
+        int     size;
+        int     *res;
+        int     i;
 
-        len = end >= start ? end - start : start - end;
-        if (!(range = (int *)malloc(sizeof(int) * len)))
+        size = end >= start ? end - start : start - end;
+        res = malloc((size + 1) * sizeof(int));
+        if (!res)
                 return (NULL);
 
-        while (end != start)
-        *range++ = end > start ? start++ : start--;
-        *range = start;
-        return (range - len);
+        i = 0;
+        if (end > start)
+        {
+                while (end >= start)
+                {
+                        res[i] = start++;
+                        i ++;
+                }
+        }
+        else
+        {
+                while (end <= start)
+                {
+                        res[i] = start--;
+                        i ++;
+                }
+        }
+        return (res);
 }
 
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+        (void)ac;
+        int     arr_len;
+        int     *arr;
+
+        arr_len = abs(atoi(av[2]) - atoi(av[1]));
+        arr = ft_range(atoi(av[1]), atoi(av[2]));
+        for (int i = 0; i <= arr_len; i += 1)
+                printf("%d\n", arr[i]);
+        free(arr);
+        return (0);
